4f6cf2f7bb6d7c440c3ad38b276b3aa530f61d1d
Joaquin Opazo <opazoes@esat-alumni.com>
2025-10-07 10:59:51 +0200

    added bat

---
+++ comp.bat
@@ -0,0 +1,8 @@
+C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvar64.bat
+
+cl.exe /c piece.cc
+cl.exe /c player.cc
+cl.exe /c parcheesi.cc
+cl.exe /c main.cc
+
+cl.exe /oparcheesi.exe *.obj
